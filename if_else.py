403bab22f93cc1e3fe7ea9e1b8d9c88818c06200
Maxramov <138771146+Maxramov@users.noreply.github.com>
2023-07-06 15:02:09 +0500

    if else
    if else haqidagi mashqlar

---
+++ if_else.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  2 12:05:19 2023
+
+@author: hp
+"""
+
+'''
+a=float(input("Juft son kiriting: "))
+if a%2==0:
+    print("Rahmat")
+else:
+    print("Bu son juft emas")
+'''
+
+'''
+a=int(input("Yoshingizni kiriting: "))
+if a<=4 or 60<=a:
+    print("Sizga kirish bepul")
+elif a<=18:
+    print("Sizga chipta 10000 so'm")
+else:
+    print("Sizga chipta 20000 so'm")
+'''
+
+'''
+#a=int(input("Birinchi sonni kiriting"))
+b=int(input("Ikkinchi sonni kiriting"))
+if a>b:
+    print(f"{a}>{b}")
+else:
+    print(f"{a}<{b}")
+''' 
+
+
+'''
+mahsulotlar=["olma", "o'rik", "tarvuz", "behi", "anjir", "shaftoli","apelsin","mandarin", "qovun", "olcha"]
+
+savat=[]
+for n in range(5):
+    savat.append(input(f"{n+1}-mahsulotni kiriting\n>>> "))
+    
+if savat:
+    for mahsulot in savat:
+        if mahsulot in mahsulotlar:
+            print(f"Do'konimizda {mahsulot} bor")
+        else:
+            print(f"Do'konimizda {mahsulot} yo'q")
+else:
+    print("Savatingiz bo'sh")
+'''
+
+
+odamlar=[123,234,345,456,567]
+login=[]
+for n in range():
+    login.append(input(f"{0}login kiriting\n>>> "))
+if login:
+    for yangi in login:
+        if yangi in odamlar:
+            print("{login} login band")
+        else:
+            print("{login} xush kelibsiz foydalanuvchi")
+
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
